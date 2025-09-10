c963d304239d49d5ed91af6431ccb9340ffa7f1e
Your Real Name <surafelmuhabaw17@gmail.com>
2025-09-10 09:28:12 +0300

    message

--- src/components/api/endPoints.tsx
+++ src/components/api/endPoints.tsx
@@ -2,13 +2,10 @@ const endPoints = {
   login: "/auth/login",
   register: "/auth/register",
   applicants: "/applicants/register",
-<<<<<<< HEAD
-=======
   createDepartmentHead: "/auth/create-department-head",
   createTeacher: "/auth/create-teacher",
   departmentHeads: "/department-heads", //comment
   teachers: "/teachers",
->>>>>>> main
 };
+
 export default endPoints;
-``;
